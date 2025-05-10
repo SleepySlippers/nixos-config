b3ec93d603b6e792b48763e09da3433494ce25d0
Arseny Staroverov <arsen2001god@bk.ru>
2025-05-10 12:08:53 +0300

    set nvim as default editor

--- home.nix
+++ home.nix
@@ -141,6 +141,11 @@
     };
   };
 
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
