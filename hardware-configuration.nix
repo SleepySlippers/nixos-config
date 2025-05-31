2475f4f9124c4866db8c5c29a7f12cb7655e25a2
Arseny Staroverov <arsen2001god@bk.ru>
2025-05-31 20:09:38 +0300

    Real hardware

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,42 +3,37 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
-let
-  pathExists = builtins.pathExists;
-  efiDevice = "/dev/disk/by-partlabel/EFI";
-  fallbackDevice = "/dev/disk/by-partlabel/ESP";
-  bootDevice = if pathExists efiDevice then efiDevice else fallbackDevice;
-in
 {
-  imports = [ ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-partlabel/root";
+    { device = "/dev/disk/by-uuid/3f2c3f1d-c92a-4fdf-9b78-a819e6392e91";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = bootDevice;
+    { device = "/dev/disk/by-uuid/AEB0-4042";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/bda11584-13a1-4562-bed9-09841f489837"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  virtualisation.virtualbox.guest.enable = true;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
